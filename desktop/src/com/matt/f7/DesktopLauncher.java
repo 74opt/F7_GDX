30f8e0929c55a389e0848ebaa632052fcb3c13e1
74opt <potatomatt0505@gmail.com>
2022-04-05 21:10:31 -0400

    Initial commit

---
+++ desktop/src/com/matt/f7/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.matt.f7;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.matt.f7.Main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main(String[] args) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new Main(), config);
+	}
+}
